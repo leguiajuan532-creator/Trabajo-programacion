05d3b9ea80a45ccd00bcbadbeb016e5a2af161ea
leguiajuan532 <leguiajuan532@gmail.com>
2026-08-26 22:03:25 -0500

    presentacion de taller

---
+++ Taller.py
@@ -0,0 +1,12 @@
+# ejercicio1.py
+nombre = "Laura Martínez"
+edad = 19
+semestre = 2
+programa = "Ingeniería de Sistemas"
+promedio = 4.3
+
+print(f"Nombre: (nombre)")
+print(f"Edad: (edad)")
+print(f"Semestre: (semestre)")
+print(f"Programa: (programa))
+print(f"Promedio: (promedio)")
